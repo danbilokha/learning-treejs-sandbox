06d102d516bd00eb5796615c77f14f5120fd83fb
danbilokha <danbilokha@gmail.com>
2021-04-25 16:30:58 +0200

    6.1

--- src/dan/main.js
+++ src/dan/main.js
@@ -13,38 +13,50 @@ import nz from "./assets/cubemap/nz.jpeg";
 import px from "./assets/cubemap/px.jpeg";
 import py from "./assets/cubemap/py.jpeg";
 import pz from "./assets/cubemap/pz.jpeg";
+import scratch from './assets/textures/scratch.jpg'
 
 console.log("Needed so it works", OrbitControls);
 
+var GEO_TYPES = [
+	'box', 
+	'cone', 
+	'cylinder', 
+	'octahedron', 
+	'sphere',
+	'tetrahedron',
+	'torus',
+	'torusKnot'
+];
+
 function init() {
-  var scene = new THREE.Scene();
-  var gui = new dat.GUI();
+	var scene = new THREE.Scene();
+	var clock = new THREE.Clock();
 
-  // initialize objects
-  var sphereMaterial = getMaterial("standard", "rgb(255, 255, 255)");
-  var sphere = getSphere(sphereMaterial, 1, 24);
+	// initialize objects
+	var objMaterial = getMaterial('basic', 'rgb(255, 255, 255)');
+	GEO_TYPES.forEach(function(type) {
+		var geo = getGeometry(type, 5, objMaterial);
+		scene.add(geo);
+	});
 
-  var planeMaterial = getMaterial("standard", "rgb(255, 255, 255)");
-  var plane = getPlane(planeMaterial, 500);
+	var lightLeft = getSpotLight(1, 'rgb(255, 220, 180)');
+	var lightRight = getSpotLight(1, 'rgb(255, 220, 180)');
+	var lightBottom = getPointLight(0.33, 'rgb(255, 220, 150)');
 
-  var lightLeft = getSpotLight(1, "rgb(255, 220, 180)");
-  var lightRight = getSpotLight(1, "rgb(255, 220, 180)");
+	lightLeft.position.x = -5;
+	lightLeft.position.y = 2;
+	lightLeft.position.z = -4;
 
-  // manipulate objects
-  sphere.position.y = sphere.geometry.parameters.radius;
-  plane.rotation.x = Math.PI / 2;
+	lightRight.position.x = 5;
+	lightRight.position.y = 2;
+	lightRight.position.z = -4;
 
-  lightLeft.position.x = -5;
-  lightLeft.position.y = 2;
-  lightLeft.position.z = -4;
+	lightBottom.position.x = 0;
+	lightBottom.position.y = 10;
+	lightBottom.position.z = 0;
 
-  lightRight.position.x = 5;
-  lightRight.position.y = 2;
-  lightRight.position.z = -4;
-
-  // manipulate materials
-  // load the cube map
-  var reflectionCube = new THREE.CubeTextureLoader().load([
+	// load the environment map
+	var reflectionCube = new THREE.CubeTextureLoader().load([
     px,
     nx,
     py,
@@ -52,148 +64,171 @@ function init() {
     pz,
     nz
   ]);
-  reflectionCube.format = THREE.RGBFormat;
+	scene.background = reflectionCube;
 
-  scene.background = reflectionCube;
-  
-  const loader = new THREE.TextureLoader();
+	// manipulate materials
+	// var loader = new THREE.TextureLoader();
+	// objMaterial.roughnessMap = loader.load(scratch);
+	// objMaterial.bumpMap = loader.load(scratch);
+	// objMaterial.bumpScale = 0.01;
+	// objMaterial.envMap = reflectionCube;
 
-  planeMaterial.map = loader.load(concrete);
-  planeMaterial.roughnessMap = loader.load(concrete);
-  planeMaterial.bumpMap = loader.load(concrete);
-  planeMaterial.bumpScale = 0.01;
-  planeMaterial.metalness = 0.1;
-  planeMaterial.roughness = 0.4;
-  planeMaterial.envMap = reflectionCube;
+	// objMaterial.roughness = 0.5;
+	// objMaterial.metalness = 0.7;
 
-  sphereMaterial.roughness = 0.2;
-  sphereMaterial.metalness = 0.4;
-  sphereMaterial.roughnessMap = loader.load(fingerprints);
-  sphereMaterial.envMap = reflectionCube;
+	// var maps = ['bumpMap', 'roughnessMap'];
+	// maps.forEach(function(map) {
+	// 	var texture = objMaterial[map];
+	// 	texture.wrapS = THREE.RepeatWrapping;
+	// 	texture.wrapT = THREE.RepeatWrapping;
+	// 	texture.repeat.set(1, 1);
+	// });
 
-  const maps = ['map', 'bumpMap', 'roughnessMap'];
-  maps.forEach((map) => {
-    const texture = planeMaterial[map];
-    texture.wrapS = THREE.RepeatWrapping;
-    texture.wrapT = THREE.RepeatWrapping;
-    texture.repeat.set(15, 15);
-  });
+	// add other objects to the scene
+	scene.add(lightLeft);
+	scene.add(lightRight);
+	scene.add(lightBottom);
 
-  // dat.gui
-  var folder1 = gui.addFolder("light_1");
-  folder1.add(lightLeft, "intensity", 0, 10);
-  folder1.add(lightLeft.position, "x", -5, 15);
-  folder1.add(lightLeft.position, "y", -5, 15);
-  folder1.add(lightLeft.position, "z", -5, 15);
+	// camera
+	var cameraGroup = new THREE.Group();
+	var camera = new THREE.PerspectiveCamera(
+		45, // field of view
+		window.innerWidth / window.innerHeight, // aspect ratio
+		1, // near clipping plane
+		1000 // far clipping plane
+	);
+	camera.position.z = 20;
+	camera.position.x = 0;
+	camera.position.y = 5;
+	camera.lookAt(new THREE.Vector3(0, 0, 0));
+	cameraGroup.add(camera);
+	cameraGroup.name = 'sceneCameraGroup';
+	scene.add(cameraGroup);
 
-  var folder2 = gui.addFolder("light_2");
-  folder2.add(lightRight, "intensity", 0, 10);
-  folder2.add(lightRight.position, "x", -5, 15);
-  folder2.add(lightRight.position, "y", -5, 15);
-  folder2.add(lightRight.position, "z", -5, 15);
+	// renderer
+	var renderer = new THREE.WebGLRenderer();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.shadowMap.enabled = true;
+	renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+	document.getElementById('webgl').appendChild(renderer.domElement);
+	update(renderer, scene, camera, clock);
 
-  var folder3 = gui.addFolder("materials");
-  folder3.add(sphereMaterial, "roughness", 0, 1);
-  folder3.add(planeMaterial, "roughness", 0, 1);
-  folder3.add(sphereMaterial, "metalness", 0, 1);
-  folder3.add(planeMaterial, "metalness", 0, 1);
-  folder3.open();
-
-  // add objects to the scene
-  scene.add(sphere);
-  scene.add(plane);
-  scene.add(lightLeft);
-  scene.add(lightRight);
-
-  // camera
-  var camera = new THREE.PerspectiveCamera(
-    45, // field of view
-    window.innerWidth / window.innerHeight, // aspect ratio
-    1, // near clipping plane
-    1000 // far clipping plane
-  );
-  camera.position.z = 7;
-  camera.position.x = -2;
-  camera.position.y = 7;
-  camera.lookAt(new THREE.Vector3(0, 0, 0));
-
-  // renderer
-  var renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.shadowMap.enabled = true;
-  document.getElementById("webgl").appendChild(renderer.domElement);
-
-  var controls = new THREE.OrbitControls(camera, renderer.domElement);
-
-  update(renderer, scene, camera, controls);
-
-  return scene;
+	return scene;
 }
 
-function getSphere(material, size, segments) {
-  var geometry = new THREE.SphereGeometry(size, segments, segments);
-  var obj = new THREE.Mesh(geometry, material);
-  obj.castShadow = true;
+function getGeometry(type, size, material) {
+	var geometry;
+	var segmentMultiplier = 0.25;
 
-  return obj;
+	switch (type) {
+		case 'box':
+			geometry = new THREE.BoxGeometry(size, size, size);
+			break;
+		case 'cone':
+			geometry = new THREE.ConeGeometry(size, size, 256*segmentMultiplier);
+			break;
+		case 'cylinder':
+			geometry = new THREE.CylinderGeometry(size, size, size, 32*segmentMultiplier);
+			break;
+		case 'octahedron':
+			geometry = new THREE.OctahedronGeometry(size);
+			break;
+		case 'sphere':
+			geometry = new THREE.SphereGeometry(size, 32*segmentMultiplier, 32*segmentMultiplier);
+			break;
+		case 'tetrahedron':
+			geometry = new THREE.TetrahedronGeometry(size);
+			break;
+		case 'torus':
+			geometry = new THREE.TorusGeometry(size/2, size/4, 16*segmentMultiplier, 100*segmentMultiplier);
+			break;
+		case 'torusKnot':
+			geometry = new THREE.TorusKnotGeometry(size/2, size/6, 256*segmentMultiplier, 100*segmentMultiplier);
+			break;
+		default:
+			break;
+	}
+
+	var obj = new THREE.Mesh(geometry, material);
+	obj.castShadow = true;
+	obj.name = type;
+
+	return obj;
 }
 
 function getMaterial(type, color) {
-  var selectedMaterial;
-  var materialOptions = {
-    color: color === undefined ? "rgb(255, 255, 255)" : color,
-  };
+	var selectedMaterial;
+	var materialOptions = {
+		color: color === undefined ? 'rgb(255, 255, 255)' : color,
+    wireframe: true
+	};
 
-  switch (type) {
-    case "basic":
-      selectedMaterial = new THREE.MeshBasicMaterial(materialOptions);
-      break;
-    case "lambert":
-      selectedMaterial = new THREE.MeshLambertMaterial(materialOptions);
-      break;
-    case "phong":
-      selectedMaterial = new THREE.MeshPhongMaterial(materialOptions);
-      break;
-    case "standard":
-      selectedMaterial = new THREE.MeshStandardMaterial(materialOptions);
-      break;
-    default:
-      selectedMaterial = new THREE.MeshBasicMaterial(materialOptions);
-      break;
-  }
+	switch (type) {
+		case 'basic':
+			selectedMaterial = new THREE.MeshBasicMaterial(materialOptions);
+			break;
+		case 'lambert':
+			selectedMaterial = new THREE.MeshLambertMaterial(materialOptions);
+			break;
+		case 'phong':
+			selectedMaterial = new THREE.MeshPhongMaterial(materialOptions);
+			break;
+		case 'standard':
+			selectedMaterial = new THREE.MeshStandardMaterial(materialOptions);
+			break;
+		default: 
+			selectedMaterial = new THREE.MeshBasicMaterial(materialOptions);
+			break;
+	}
 
-  return selectedMaterial;
+	return selectedMaterial;
+}
+
+function getPointLight(intensity, color) {
+	var light = new THREE.PointLight(color, intensity);
+	light.castShadow = true;
+
+	return light;
 }
 
 function getSpotLight(intensity, color) {
-  color = color === undefined ? "rgb(255, 255, 255)" : color;
-  var light = new THREE.SpotLight(color, intensity);
-  light.castShadow = true;
-  light.penumbra = 0.5;
+	color = color === undefined ? 'rgb(255, 255, 255)' : color;
+	var light = new THREE.SpotLight(color, intensity);
+	light.castShadow = true;
+	light.penumbra = 0.5;
 
-  //Set up shadow properties for the light
-  light.shadow.mapSize.width = 2048; // default: 512
-  light.shadow.mapSize.height = 2048; // default: 512
-  light.shadow.bias = 0.001;
+	//Set up shadow properties for the light
+	light.shadow.mapSize.width = 1024;  // default: 512
+	light.shadow.mapSize.height = 1024; // default: 512
+	light.shadow.camera.near = 0.1;       // default
+	light.shadow.camera.far = 500      // default
+	light.shadow.camera.fov = 30      // default
+	light.shadow.bias = 0.001;
 
-  return light;
+	return light;
 }
 
-function getPlane(material, size) {
-  var geometry = new THREE.PlaneGeometry(size, size);
-  material.side = THREE.DoubleSide;
-  var obj = new THREE.Mesh(geometry, material);
-  obj.receiveShadow = true;
+function update(renderer, scene, camera, clock) {
+	// rotate camera around the origin
+	var sceneCameraGroup = scene.getObjectByName('sceneCameraGroup');
+	if (sceneCameraGroup) {
+		sceneCameraGroup.rotation.y += 0.005;
+	}
 
-  return obj;
-}
+  var currentIndex = Math.floor((clock.getElapsedTime() / 4) % GEO_TYPES.length);
+	GEO_TYPES.forEach(function(geo, index) {
+		var currentObj = scene.getObjectByName(geo);
+		if (index === currentIndex) {
+			currentObj.visible = true;
+		} else {
+			currentObj.visible = false;
+		}
+	});
 
-function update(renderer, scene, camera, controls) {
-  controls.update();
-  renderer.render(scene, camera);
-  requestAnimationFrame(function () {
-    update(renderer, scene, camera, controls);
-  });
+	renderer.render(scene, camera);
+	requestAnimationFrame(function() {
+		update(renderer, scene, camera, clock);
+	});
 }
 
 var scene = init();
